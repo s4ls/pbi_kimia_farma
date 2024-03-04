1c0247c9e91a37e462f22b585b6e236eed740268
s4ls <162033588+s4ls@users.noreply.github.com>
2024-03-04 17:18:57 +0700

    Add files via upload

---
+++ Agregate_Profit setiap provinsi.sql
@@ -0,0 +1,29 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2759
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red39\green78\blue204;\red255\green255\blue255;\red44\green55\blue61;
+\red0\green0\blue0;\red42\green55\blue62;}
+{\*\expandedcolortbl;;\cssrgb\c20000\c40392\c83922;\cssrgb\c100000\c100000\c100000;\cssrgb\c22745\c27843\c30588;
+\cssrgb\c0\c0\c0;\cssrgb\c21569\c27843\c30980;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 SELECT\cf4 \cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf4 \cb3   \strokec5 provinsi\strokec4 ,\cb1 \
+\cb3   \cf2 \strokec2 SUM\cf6 \strokec6 (\cf4 \strokec5 nett_profit\cf6 \strokec6 )\cf4 \strokec4  \cf2 \strokec2 AS\cf4 \strokec4  \strokec5 total_profit\cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf2 \cb3 \strokec2 FROM\cf4 \cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf4 \cb3   \strokec5 dataset_kimia_farma.kf_analisa1\cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf2 \cb3 \strokec2 GROUP\cf4 \strokec4  \cf2 \strokec2 BY\cf4 \cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf4 \cb3   \strokec5 provinsi\cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf2 \cb3 \strokec2 ORDER\cf4 \strokec4  \cf2 \strokec2 BY\cf4 \cb1 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf4 \cb3   \strokec5 total_profit\strokec4  \cf2 \strokec2 DESC\cf4 \strokec4 ;\cb1 \
+\
+}
